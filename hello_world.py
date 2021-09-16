cce7436b484f3eaa625613a7a1caa1b11e7e7fe4
KennethBlaney <kenneth.r.blaney@gmail.com>
2021-09-16 19:25:57 -0400

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,11 @@
-# This is a test comment.
-print("Hello World!")
-print("How are you?!")
-print("A new line")
+from flask import Flask
+app = Flask(__name__) # "__main__"
+
+
+@app.route('/hello', methods=['GET'])
+def hello_world():
+  return "Hello world."
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=8080)
+  
